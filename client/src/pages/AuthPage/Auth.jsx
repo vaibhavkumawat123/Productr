dc35f44680eb89db338ec9eb5846a51a9765627e
Vaibhav <vaibhavraj771@gmail.com>
2025-12-15 14:54:23 +0530

    update

--- client/src/pages/AuthPage/Auth.jsx
+++ client/src/pages/AuthPage/Auth.jsx
@@ -11,7 +11,9 @@ const Auth = () => {
   const { token, loading } = useAuth();
 
   const [mode, setMode] = useState("login");
+
   const [step, setStep] = useState(1);
+
   const [email, setEmail] = useState("");
   const [signupData, setSignupData] = useState(null);
 
@@ -21,35 +23,51 @@ const Auth = () => {
 
   return (
     <div className="w-full h-screen flex bg-[#f5f7ff]">
-      {/* LEFT */}
+      {/* LEFT IMAGE */}
       <div className="w-1/2 h-full relative">
         <img
           src={authImage}
+          alt="Auth"
           className="absolute inset-0 w-full h-full object-cover"
         />
       </div>
 
-      {/* RIGHT */}
+      {/* RIGHT CONTENT */}
       <div className="w-1/2 flex flex-col justify-center items-center">
+        {/*  LOGIN  */}
         {step === 1 && mode === "login" && (
           <LoginForm
             setEmail={setEmail}
             onOtpSent={() => setStep(2)}
-            switchMode={() => setMode("signup")}
+            switchMode={() => {
+              setMode("signup");
+              setStep(1); 
+              setSignupData(null); 
+            }}
           />
         )}
 
+        {/*  SIGNUP  */}
         {step === 1 && mode === "signup" && (
           <SignupForm
             setEmail={setEmail}
             setSignupData={setSignupData}
             onOtpSent={() => setStep(2)}
-            switchMode={() => setMode("login")}
+            switchMode={() => {
+              setMode("login");
+              setStep(1); 
+              setSignupData(null);
+            }}
           />
         )}
 
+        {/*  OTP VERIFY  */}
         {step === 2 && (
-          <OtpVerify email={email} mode={mode} signupData={signupData} />
+          <OtpVerify
+            email={email}
+            mode={mode} 
+            signupData={signupData}
+          />
         )}
       </div>
     </div>
