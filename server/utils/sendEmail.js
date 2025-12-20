b7af595ece9a53cab9af5bc5b585c4b46cf6f10d
Vaibhav <vaibhavraj771@gmail.com>
2025-12-20 22:21:46 +0530

    updated

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -6,28 +6,22 @@ import nodemailer from "nodemailer";
 console.log("EMAIL_USER =", process.env.EMAIL_USER);
 console.log("EMAIL_PASS =", process.env.EMAIL_PASS ? "LOADED" : "MISSING");
 
-const sendEmail = async (to, subject, html) => {
-  try {
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-      },
-    });
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS, 
+  },
+});
 
-    await transporter.sendMail({
-      from: `"Productr" <${process.env.EMAIL_USER}>`,
-      to,
-      subject,
-      html,
-    });
-
-    console.log("📩 Email sent to", to);
-  } catch (err) {
-    console.error("❌ EMAIL ERROR:", err);
-    throw err;
-  }
+const sendEmail = async (to, subject, text) => {
+  await transporter.sendMail({
+    from: process.env.EMAIL_USER,
+    to,
+    subject,
+    subject,
+    text,
+  });
 };
 
 export default sendEmail;
